3c49bc0997293236f63a7f5d61aefd439b17a494
soubj <sobujahmed124@gmail.com>
2023-05-01 22:53:30 +0600

    added firebase to the project using npm install firebase --force

---
+++ src/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDH85PjpsGTP9rnUnfe7QMLe5TNg8j55_o",
+  authDomain: "simple-firebase-9fcd7.firebaseapp.com",
+  projectId: "simple-firebase-9fcd7",
+  storageBucket: "simple-firebase-9fcd7.appspot.com",
+  messagingSenderId: "586103925525",
+  appId: "1:586103925525:web:8e30e33fc755928cba971c",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
