9eba9cfc2f2cd8118a40c4e10e86110e775bed69
soubj <sobujahmed124@gmail.com>
2023-05-02 21:27:09 +0600

    showed data after firebase login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,16 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import app from "../../firebase.init";
 import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
 
 const Login = () => {
+  const [user, setUser] = useState(null);
   const auth = getAuth(app);
   const provider = new GoogleAuthProvider();
 
   const handleGoogleSignIn = () => {
     signInWithPopup(auth, provider)
       .then((result) => {
-        const user = result.user;
-        console.log(user);
+        const loggedInUser = result.user;
+        console.log(loggedInUser);
+        setUser(loggedInUser);
       })
       .catch((error) => {
         console.log(error);
@@ -20,6 +22,14 @@ const Login = () => {
   return (
     <div>
       <button onClick={handleGoogleSignIn}>log in</button>
+      {/* conditional rendering */}
+      {user && (
+        <div>
+          <h3> User : {user.displayName}</h3>
+          <h4>Email : {user.email}</h4>
+          <img src={user.photoURL} alt="" />
+        </div>
+      )}
     </div>
   );
 };
